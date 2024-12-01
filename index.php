ca03634ac8c521cc7522d1975dd214182023c8f8
Alex Santana <aleximbw@gmail.com>
2024-12-01 18:15:13 -0300

    Mudanças importantes
    Criação da home
    Mudança no estilo visual
    Criação de arquivo js
    hidden em inputs

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    
     <title>Sistema</title>
 </head>
 <body>
@@ -15,18 +16,30 @@
 
         <form action="cria_usuario.php" method="post">
 
-            <input type="text" name="nome" class="dados" placeholder="Nome">
+            <input type="text" name="nome" class="dados" placeholder="E-mail">
         
             <br>
             <br>
         
             <input type="password" name="passw" class="dados" placeholder="Senha">
+
+            <br>
+            <br>
+
+            <input type="password" name="confpassw" class="dados" placeholder="Confirmar senha">
         
             <br>
             <br>
             <hr>
         
             <input type="submit" value="Cadastrar" id="entrar">
+            <input type="submit" value="Entrar" hidden>
+
+            <br>
+            <br>
+
+            <a href="">Fazer login</a>
+            <a href="" hidden>Cadastrar-se</a>
         </form>
     </div>
     
